df5dd4c5c02fcc29bc0e393259e1e6ad7bafd689
travisen <travis.nykaza@gmail.com>
2017-06-20 15:13:42 -0400

    Connect to postgresql

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var app = express();
+var pg = require("pg")
+
 
 app.set("view engine", "ejs");
 
@@ -18,6 +20,20 @@ app.get("/*", function(req, res){
     // res.send("Index page!");
     res.render("not-found");
 });
+
+//Test db
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
+
 
 app.listen(process.env.PORT || 3000 , function(){
     if (process.env.PORT != null){
