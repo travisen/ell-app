d83ec847f4dc125d790982e3a278d94595db812e
travisen <travis.nykaza@gmail.com>
2017-06-20 14:02:01 -0400

    Add more routes

--- app.js
+++ app.js
@@ -8,6 +8,17 @@ app.get("/", function(req, res){
     res.render("landing");
 });
 
+app.get("/admin", function(req, res){
+    // res.send("Index page!");
+    res.render("admin");
+});
+
+// Page does not exist
+app.get("/*", function(req, res){
+    // res.send("Index page!");
+    res.render("not-found");
+});
+
 app.listen(3000, "localhost" ,function(){
     console.log("Server started on port 3000");
 });
