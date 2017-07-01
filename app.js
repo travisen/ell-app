8484406a2151adf03ab99c2e9fa51b08c5c6809b
travisen <travis.nykaza@gmail.com>
2017-07-01 01:22:15 -0400

    Add connection pool to db

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-var express = require("express");
-var app = express();
-var pg = require("pg")
+var express       = require("express"),
+    app           = express(),
+    pg            = require("pg");
+
+//Setup DB pool
+const Pool        = require("pg-pool"),
+      url         = require("url"),
+      params = url.parse(process.env.DATABASE_URL),
+      auth = params.auth.split(":");
+
+const config = {
+  user: auth[0],
+  password: auth[1],
+  host: params.hostname,
+  port: params.port,
+  database: params.pathname.split("/")[1],
+  ssl: true
+};
+
+const pool = new Pool(config);
 
 app.set("view engine", "ejs");
 
@@ -52,27 +69,52 @@ app.get("/admin", function(req, res){
 
 
 //Test db
+// app.get('/db', function (request, response) {
+
+//   pg.defaults.ssl = true;
+
+//   pg.connect(process.env.DATABASE_URL, function(err, client) {
+//     if (err) throw err;
+//     console.log('Connected to postgres! Getting data from users:');
+
+//     client.query('SELECT * FROM person;', function(err, result) {
+//       if(err) {
+//         console.error(err);
+//         response.send("Error" + err);
+//       }
+//       else {
+//         response.send({results: result.rows});
+//       }
+//     });
+//   });
+  
+// });
+//Test db
+
 app.get('/db', function (request, response) {
 
-  pg.defaults.ssl = true;
+  //  pool.connect().then(client => {
+  //   client.query('SELECT * FROM person;', function(err, result) {
+  //     if(err) {
+  //       console.error(err);
+  //       response.send("Error" + err);
+  //     }
+  //     else {
+  //       response.send({results: result.rows});
+  //     }
+  //   });
+  // });
 
-  pg.connect(process.env.DATABASE_URL, function(err, client) {
-    if (err) throw err;
-    console.log('Connected to postgres! Getting data from users:');
+  pool.query('SELECT * FROM person;', function(err, result) {
+    if(err){
+      console.error(err);
+      response.send("ERROR" + err);
+    } else {
+      response.send({results: result.rows});
+    }
 
-    client.query('SELECT * FROM person;', function(err, result) {
-      if(err) {
-        console.error(err);
-        response.send("Error" + err);
-      }
-      else {
-        response.send({results: result.rows});
-      }
     });
-  });
-  
 });
-
 
 // Page does not exist
 app.get("/*", function(req, res){
